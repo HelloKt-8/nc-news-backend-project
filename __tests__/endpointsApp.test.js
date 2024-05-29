c137f1f072c4811194481d65a347be70c9075363
Katie Luc <k_luc@hotmail.co.uk>
2024-05-29 14:20:08 +0100

    FEEDBACK: updates the endpoint test to use fs.promises

--- __tests__/endpointsApp.test.js
+++ __tests__/endpointsApp.test.js
@@ -1,22 +1,23 @@
 const request = require("supertest");
 const endpointsApp = require("../endpointsApp");
 const fs = require("fs");
-const path = require("path");
 
 describe("GET/api", () => {
-  test("should respond with JSON object describing all endpoints", () => {
-    fs.readFile("endpoints.json", "utf-8", (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        return;
-      }
-      const expected = JSON.parse(data);
-      return request(endpointsApp)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toEqual(expected);
+    test("should respond with JSON object describing all endpoints", () => {
+      return fs.promises.readFile("endpoints.json", "utf-8")
+        .then(data => {
+          const expected = JSON.parse(data);
+  
+          return request(endpointsApp)
+            .get("/api")
+            .expect(200)
+            .then(response => {
+              expect(response.body).toEqual(expected);
+            });
+        })
+        .catch(err => {
+          throw err; 
         });
     });
   });
-});
+  
